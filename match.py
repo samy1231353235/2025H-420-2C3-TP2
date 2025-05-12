cb018159610d85e1879efb06c114fb373f9ebcc2
samym <samymerrouchi999@gmail.com>
2025-05-11 22:24:26 -0400

    changements a match

--- match.py
+++ match.py
@@ -1,17 +1,18 @@
-# match.py
-
 class Match:
     def __init__(self, joueur1, joueur2):
+        # crée un match avec 2 joueurs et des scores à 0
         self.joueur1 = joueur1
         self.joueur2 = joueur2
         self.score1 = 0
         self.score2 = 0
 
     def definir_scores(self, score1, score2):
+        # enregistre les scores du match
         self.score1 = score1
         self.score2 = score2
 
     def gagnant(self):
+        # retourne le gagnant du match
         if self.score1 > self.score2:
             return self.joueur1
         elif self.score2 > self.score1:
