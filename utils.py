01ef73e5e380814b8c1e40e0524e28d04970af9c
samym <samymerrouchi999@gmail.com>
2025-05-11 22:22:48 -0400

    changements a utils

--- utils.py
+++ utils.py
@@ -2,24 +2,18 @@ import csv
 import json
 
 def lire_csv(chemin):
-    """
-    Lire un fichier CSV et retourner la liste des lignes.
-    Chaque liste représente une ligne du fichier (sans l’en-tête).
-    """
+    # lit un fichier csv et retourne les lignes (sauf l'en-tête)
     with open(chemin, mode='r', newline='', encoding='utf-8') as f:
         lecteur = csv.reader(f)
-        next(lecteur)  # Ignorer l'en-tête
+        next(lecteur)  # saute la première ligne
         return [ligne for ligne in lecteur]
+
 def sauvegarder_json(objet, chemin):
-    """
-    Sauvegarder un objet Python dans un fichier JSON.
-    """
+    # sauvegarde un objet dans un fichier json
     with open(chemin, 'w', encoding='utf-8') as f:
         json.dump(objet, f, ensure_ascii=False, indent=4)
 
 def ecrire_texte(texte, chemin):
-    """
-    Écrire une chaîne de caractères dans un fichier texte.
-    """
+    # écrit du texte dans un fichier texte
     with open(chemin, 'w', encoding='utf-8') as f:
         f.write(texte)
