02b9708be0d6013c9a485fe697db2ffbd7ad55e5
samym <samymerrouchi999@gmail.com>
2025-05-11 22:26:19 -0400

    changements a joueur

--- joueur.py
+++ joueur.py
@@ -1,14 +1,15 @@
-# joueur.py test
-
 class Joueur:
     def __init__(self, pseudo):
+        # crée un joueur avec un pseudo et 0 victoire
         self.pseudo = pseudo
         self.victoires = 0
 
     def enregistrer_victoire(self):
+        # ajoute 1 victoire
         self.victoires += 1
 
     def to_dict(self):
+        # retourne les infos du joueur en dictionnaire
         return {
             "pseudo": self.pseudo,
             "victoires": self.victoires
@@ -16,6 +17,7 @@ class Joueur:
 
     @staticmethod
     def from_dict(data):
+        # crée un joueur à partir d'un dictionnaire
         joueur = Joueur(data["pseudo"])
         joueur.victoires = data.get("victoires", 0)
         return joueur
